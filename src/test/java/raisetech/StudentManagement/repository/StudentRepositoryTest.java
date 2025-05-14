9747f20e8e1d4a1a966adcc00426887ff82e13af
morishita-e <l1812369968as@gmail.com>
2025-05-14 22:36:06 +0900

    スタンダート第52回問題発生

---
+++ src/test/java/raisetech/StudentManagement/repository/StudentRepositoryTest.java
@@ -0,0 +1,14 @@
+package raisetech.StudentManagement.repository;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@MybatisTest
+class StudentRepositoryTest {
+
+  @Autowired
+  private StudentRepository sut;
+
+
+}
