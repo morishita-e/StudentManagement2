f82d29a78b1700af1e862a199793d63ff57eed60
morishita-e <l1812369968as@gmail.com>
2025-07-24 16:30:51 +0900

    Update data.sql

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -15,41 +15,41 @@ INSERT INTO students (name, kana_name, nickname, email, area, age, sex, remark, 
 ('古川正樹',   'フルカワマサキ',   'マサ',   'masa@example.com',    '徳島',   17, '男性',   NULL,     0);
 
 INSERT INTO students_courses
-(studentId, courseName, courseStartAt, courseEndAt, applicationStatus)
+(student_id, course_name, course_start_at, course_end_at, application_status)
 VALUES (5, '宅建士(宅地建物取引士)', '2024-10-01 00:00:00', '2025-11-30 23:59:59', '仮申込');
 
 INSERT INTO students_courses
-(studentId, courseName, courseStartAt, courseEndAt, applicationStatus)
+(student_id, course_name, course_start_at, course_end_at, application_status)
 VALUES (2, 'JAVAベーシック', '2024-11-23 10:30:00', '2025-03-31 23:59:59', '仮申込');
 
 INSERT INTO students_courses
-(studentId, courseName, courseStartAt, courseEndAt, applicationStatus)
+(student_id, course_name, course_start_at, course_end_at, application_status)
 VALUES (2, 'JAVAスタンダード', '2024-11-23 10:30:00', '2025-03-31 23:59:59', '仮申込');
 
 INSERT INTO students_courses
-(studentId, courseName, courseStartAt, courseEndAt, applicationStatus)
+(student_id, course_name, course_start_at, course_end_at, application_status)
 VALUES (3, '司法試験-裁判官コース', '2024-08-11 15:30:00', '2025-08-11 23:59:59', '仮申込');
 
 INSERT INTO students_courses
-(studentId, courseName, courseStartAt, courseEndAt, applicationStatus)
+(student_id, course_name, course_start_at, course_end_at, application_status)
 VALUES (4, 'JAVAスタンダード', '2025-01-17 17:00:00', '2025-06-30 23:59:59', '仮申込');
 
 INSERT INTO students_courses
-(studentId, courseName, courseStartAt, courseEndAt, applicationStatus)
+(student_id, course_name, course_start_at, course_end_at, application_status)
 VALUES (9, '社会マーケティング', '2025-03-13 17:52:44', '2026-03-13 17:52:44', '仮申込');
 
 INSERT INTO students_courses
-(studentId, courseName, courseStartAt, courseEndAt, applicationStatus)
+(student_id, course_name, course_start_at, course_end_at, application_status)
 VALUES (10, 'AWSコース', '2025-04-03 18:53:47', '2026-04-03 18:53:47', '仮申込');
 
 INSERT INTO students_courses
-(studentId, courseName, courseStartAt, courseEndAt, applicationStatus)
+(student_id, course_name, course_start_at, course_end_at, application_status)
 VALUES (11, '国家公務員コース', '2025-04-07 16:12:49', '2026-04-07 16:12:49', '仮申込');
 
 INSERT INTO students_courses
-(studentId, courseName, courseStartAt, courseEndAt, applicationStatus)
+(student_id, course_name, course_start_at, course_end_at, application_status)
 VALUES (12, '電子技師コース', '2025-04-07 16:26:46', '2026-04-07 16:26:46', '仮申込');
 
 INSERT INTO students_courses
-(studentId, courseName, courseStartAt, courseEndAt, applicationStatus)
+(student_id, course_name, course_start_at, course_end_at, application_status)
 VALUES (13, '簿記コース', '2025-04-07 16:31:46', '2026-04-07 16:31:46', '仮申込');
